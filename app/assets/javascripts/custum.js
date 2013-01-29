4d4454f9709ab87941ff4fc73ace54bfc7de6225
Nitin Grover <niting@fizzysoftware.com>
2013-01-29 14:20:37 +0530

    top nav

---
+++ app/assets/javascripts/custum.js
@@ -0,0 +1,14 @@
+/*$(function () {
+	    $.jBar({
+	        type: 'fixed', // fixed/static
+	        delay: '1000', // Entrance delay (milliseconds)
+	        backgroundColor: '#DB5903', // Background Color
+	        borderColor: '#FFF', // Background Color
+	        buttonTextColor: '#FFF', // Button Text
+	        buttonColor: '#333', // Button Color
+	        buttonColorHover: '#222', // Button Color Hover
+	        calltoAction: 'jBar Plugin! A simple and lightweight notification banner.', // Call to action text
+	        buttonText: 'Download it!', // Button Text
+	        buttonLink: 'http://www.toddmotto.com/jbar-plugin-the-jquery-call-to-action-bar' // Hyperlink from button
+	    });
+	});*/

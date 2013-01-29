48ea346eddf7aeb549dee95d7f084312cad65a47
manoj <manoj@fizzysoftware.com>
2013-01-29 17:13:27 +0530

    Bar styling feature implemented

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -2,8 +2,4 @@ class Bar < ActiveRecord::Base
   attr_accessible :active, :link, :link_url, :message, :name, :text_color, :bg_color, :link_text_color, :link_bg_color
 
   belongs_to :user
-
-  def self.find_with_sendable_fields( _id )
-    select( [:link, :link_url, :message]).find( _id)
-  end
 end

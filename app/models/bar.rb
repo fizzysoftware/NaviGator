30b82efcc12dd8312fb8564c6e7567d1f4dae0cf
manoj <manoj@fizzysoftware.com>
2013-01-29 14:15:56 +0530

    Color picker attached in styling fields

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,5 +1,5 @@
 class Bar < ActiveRecord::Base
-  attr_accessible :active, :link, :link_url, :message, :name
+  attr_accessible :active, :link, :link_url, :message, :name, :text_color, :bg_color, :link_text_color, :link_bg_color
 
   belongs_to :user
 

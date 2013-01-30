11ad1a1f0474b369c461f9171d488268ee64f147
manoj <manoj@fizzysoftware.com>
2013-01-30 11:45:10 +0530

    validation included in Bar model

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -2,4 +2,6 @@ class Bar < ActiveRecord::Base
   attr_accessible :active, :link, :link_url, :message, :name, :text_color, :bg_color, :link_text_color, :link_bg_color
 
   belongs_to :user
+
+  validates :link, :link_url, :message, :name, presence: true
 end

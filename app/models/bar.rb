438ab8488c8895eb3870cc2876b7c21c27a4db3f
manoj <manoj@fizzysoftware.com>
2013-02-05 11:48:23 +0530

    by default bar is active and not shown for new bar

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -6,11 +6,18 @@ class Bar < ActiveRecord::Base
 
   validates :link, :link_url, :message, :name, presence: true
 
+  before_create :activate_bar
+
   def hits_since( _days )
     visitors.since( _days ).sum(:hits)
   end
 
   def hit_through_rate_since( _days )
     ( ( hits_since( _days ) * 100 ) / visitors.count_since( _days ) ).to_i rescue 0
+  end
+
+  protected
+  def activate_bar
+    self.active = true
   end
 end

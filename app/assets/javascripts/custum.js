96a7fd42e72e98521b3e9ea42abfdd93042f0754
Nitin Grover <niting@fizzysoftware.com>
2013-02-06 21:23:47 +0530

    jbar plugin and images

--- app/assets/javascripts/custum.js
+++ app/assets/javascripts/custum.js
@@ -1,17 +1,17 @@
-/*$(function () {
+$(function () {
 	    $.jBar({
 	        type: 'fixed', // fixed/static
 	        delay: '1000', // Entrance delay (milliseconds)
 	        backgroundColor: '#DB5903', // Background Color
-	        borderColor: '#FFF', // Background Color
+	        borderColor: '#309EEB', // Background Color
 	        buttonTextColor: '#FFF', // Button Text
 	        buttonColor: '#333', // Button Color
 	        buttonColorHover: '#222', // Button Color Hover
 	        calltoAction: 'jBar Plugin! A simple and lightweight notification banner.', // Call to action text
 	        buttonText: 'Download it!', // Button Text
-	        buttonLink: 'http://www.toddmotto.com/jbar-plugin-the-jquery-call-to-action-bar' // Hyperlink from button
+	        buttonLink: '#' // Hyperlink from button
 	    });
-	});*/
+	});
 
 function timeRangesClick( obj ) {
   var _parent = $( obj ).parent();

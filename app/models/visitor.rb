ba21288d5ee8b63b0bcf6bb4b84b2ed61cc3ebeb
manoj <manoj@fizzysoftware.com>
2013-02-06 18:13:29 +0530

    commented unused code

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -6,7 +6,6 @@ class Visitor < ActiveRecord::Base
   validates :session_id, presence: true
 
   scope :since, lambda { |_number| where( :created_at => ( _number.days.ago.end_of_day..Time.now))}
-
   scope :within_week, lambda { where( :updated_at => ( 1.week.ago..Time.now ) ) }
 
   def self.count_since( _number = nil )

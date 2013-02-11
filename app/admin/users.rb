b4025818444d710733b75d312126323f68cb952b
manoj <manoj@fizzysoftware.com>
2013-02-11 09:24:51 +0530

    admin module included

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,19 @@
 ActiveAdmin.register User do
-  
+
+  index do
+    column :name
+    column :email
+    column :sign_in_count
+    default_actions
+  end
+
+  form do |f|
+    f.inputs "User Details" do
+      f.input :email
+      f.input :name
+      f.input :password
+    end
+    f.buttons
+  end
+
 end
--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,4 +1,6 @@
 class Bar < ActiveRecord::Base
+  include ActAsCountable
+
   attr_accessible :active, :link, :link_url, :message, :name, :text_color, :bg_color, :link_text_color, :link_bg_color
 
   belongs_to :user

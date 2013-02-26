451619d29e7b56691cbc34ba7b342158b89a56ba
manoj <manoj@fizzysoftware.com>
2013-02-26 12:13:42 +0530

    timeout flash error message removed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
 	def evaluate_root_path
 		signed_in? ? user_bars_path( current_user ) : root_path
 	end
+
+  # this is needed specially with devise :timeoutable feature
+  # because devise uses the flash[:timedout] for redirection upon session time out and sets its value to true
+  # If the flash is not removed, the resulting markup looks like this after a session time out
+  # <div class="flash flash_timedout">true</div>
+  def flash_with_valid_messages
+    flash.select{|k,v| v.kind_of?( String ) }
+  end
 end

336888fe333b9b44c441da6171c1bd53aba94b29
Nikhil Kathuria <nikhilk@fizzysoftware.com>
2013-04-26 14:32:11 +0530

    css changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,20 +3,13 @@ load "deploy/assets"                    #precompile all the css, js and images..
 require "bundler/capistrano"            # install all the new missing plugins...
 require 'capistrano/ext/multistage'     # deploy on all the servers..
 require "rvm/capistrano"                # if you are using rvm on your server..
-require './config/boot'  
-#require 'thinking_sphinx/deploy/capistrano'         
-require 'delayed/recipes'               # load this for delayed job..
+require './config/boot'           
 
 before "deploy:assets:precompile","deploy:config_symlink"
-before "deploy:update_code",    "delayed_job:stop"  # stop the previous deployed job workers...
 after "deploy:update", "deploy:cleanup" #clean up temp files etc.
 after "deploy:update_code","deploy:migrate"
-#after "deploy:update_code", "thinking_sphinx:rebuild"
-after "deploy:start",   "delayed_job:start" #start the delayed job 
-after "deploy:restart", "delayed_job:restart" # restart it..
 
-set(:application) { "navigator" }
-set :delayed_job_args, "-n 2"            # number of delayed job workers 
+set(:application) { "navigator" }           # number of delayed job workers 
 set :rvm_ruby_string, '1.9.3'             # ruby version you are using...
 set :rvm_type, :user
 set :whenever_environment, defer { stage }  # whenever gem for cron jobs...          
@@ -25,7 +18,7 @@ server "192.155.90.129", :app, :web, :db, :primary => true
 set (:deploy_to) { "/home/fizzy/#{application}/#{stage}" }
 set :user, 'fizzy'
 set :keep_releases, 3
-set :repository, "git clone git@bitbucket.org:fizzysoftware/fizzybar.git"
+set :repository, "git@bitbucket.org:fizzysoftware/fizzybar.git"
 set :use_sudo, false
 set :scm, :git
 default_run_options[:pty] = true
